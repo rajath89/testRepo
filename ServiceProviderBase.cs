59c2bba0fcd1dc0885facf5f8540ea8ce7ba5c00
rajath89 <42107214+rajath89@users.noreply.github.com>
2024-06-14 16:45:37 +0530

    Update ServiceProviderBase.cs

--- ServiceProviderBase.cs
+++ ServiceProviderBase.cs
@@ -13,9 +13,20 @@ namespace ConsoleApp1
 
         protected T CreateProxyInterface<T>()
         {
-            // Assuming the endpoint address is fetched from the config
-            string endpointAddress = Config.Providers[Config.DefaultProvider].ServiceUrl;
-            var binding = new BasicHttpBinding();
+            string defaultProviderName = Config.DefaultProvider;
+            var providerSettings = Config.Providers[defaultProviderName];
+
+            string endpointAddress = providerSettings.ServiceUrl;
+            int operationTimeout = providerSettings.ServiceOperationTimeout;
+
+            var binding = new BasicHttpBinding
+            {
+                OpenTimeout = TimeSpan.FromSeconds(operationTimeout),
+                CloseTimeout = TimeSpan.FromSeconds(operationTimeout),
+                SendTimeout = TimeSpan.FromSeconds(operationTimeout),
+                ReceiveTimeout = TimeSpan.FromSeconds(operationTimeout)
+            };
+
             var endpoint = new EndpointAddress(endpointAddress);
             var channelFactory = new ChannelFactory<T>(binding, endpoint);
             return channelFactory.CreateChannel();
